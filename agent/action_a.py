8f3015d1c77ffc51c94701c108e6400734781728
Haaaaya <hayatosetoya@gmail.com>
2026-04-20 00:04:47 +0900

    update README to add instructions

--- agent/action_a.py
+++ agent/action_a.py
@@ -8,4 +8,14 @@ class ActionA:
         self.rnd = rnd
 
     def act(self, pos, a_data, e_data):
+        #　ここの中を編集しよう！
+
+        # 使える変数など
+        # pos: 自分の現在座標 例) [3, 4]
+        # a_data: 味方の情報リスト 例) [{"pos": [1, 2], "status": "active"}, ...]
+        # e_data: 敵の情報リスト   例) [{"pos": [5, 6], "status": "broken"}, ...]
+
+        # self.field.get_event(x, y): ★があれば1, なければ0
+        # 例) self.field.get_event(3, 4) -> 1
+        
         return self.rnd.randint(0, 7)
